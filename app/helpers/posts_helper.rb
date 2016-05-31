98cdc526f7720a03103d831b4be9d843754b7ac9
mo7mmad <mo7am.med@hotmail.com>
2016-05-31 17:26:30 +0300

    display likes of post and unliking

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,36 @@
 module PostsHelper
 
+  def display_likes(post)
+  votes = post.votes_for.up.by_type(User)
+  return list_likers(votes) if votes.size <= 8
+  count_likers(votes)
+end
+
+def liked_post(post)
+  return 'glyphicon-heart' if current_user.voted_for? post
+  'glyphicon-heart-empty'
+end
+
+private
+
+def list_likers(votes)
+  user_names = []
+  unless votes.blank?
+    votes.voters.each do |voter|
+      user_names.push(link_to voter.user_name,
+                              profile_path(voter.user_name),
+                              class: 'user-name')
+    end
+    user_names.to_sentence.html_safe + like_plural(votes)
+  end
+end
+
+def count_likers(votes)
+  vote_count = votes.size
+  vote_count.to_s + ' likes'
+end
+
+
   # Our new helper method
   def likers_of(post)
     # votes variable is set to the likes by users.
