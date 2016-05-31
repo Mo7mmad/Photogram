ad91a473f16f0a9494f11edf53fc25ad4e4ac67d
mo7mmad <mo7am.med@hotmail.com>
2016-05-31 11:19:07 +0300

    Add profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
+  root 'posts#index'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
+
   resources :posts do
     resources :comments
   end
-  root 'posts#index'
+  get ':user_name', to: 'profiles#show', as: :profile
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
